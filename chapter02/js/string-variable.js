6218c2412b21fb1689a40aa1e3fc5c206f2ab7a3
tom-foster <tom-foster@users.noreply.github.com>
2016-07-17 23:09:42 +0100

    string variable

---
+++ chapter02/js/string-variable.js
@@ -0,0 +1,12 @@
+//strings tf 17/07/16
+
+var username;
+var message;
+username = 'Molly';
+message = 'See our upcoming range';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+
+var elNote = document.getElementById('note');
+elNote.textContent = message;
