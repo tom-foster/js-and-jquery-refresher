121e2b4026e04ff93bdb71bb5cc76ec4625a5b7d
tom-foster <tom-foster@users.noreply.github.com>
2016-07-18 23:14:13 +0100

    boolean varaible example

---
+++ chapter02/js/boolean-variable.js
@@ -0,0 +1,12 @@
+//boolean example
+//tf 18/07/16
+var inStock;
+var Shipping;
+inStock = true;
+Shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className = Shipping;
