fb0326d124c48004b3fba548210615460e983895
tom-foster <tom-foster@users.noreply.github.com>
2016-07-20 22:52:53 +0100

    array literal and array constructor

---
+++ chapter02/js/array-constructor.js
@@ -0,0 +1,7 @@
+var colors = new Array('white',
+                        'black',
+                        'red',
+                        'custom');
+
+var el = document.getElementById('colors');
+el.textContent = colors[2];
---
+++ chapter02/js/array-literal.js
@@ -0,0 +1,6 @@
+//An array literal the preferred way of writing arrays in js
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById('colors');
+el.textContent = colors[1];
