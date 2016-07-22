41d09d54c5c53e47b5e5e41a01cfef950a68727d
tom-foster <tom-foster@users.noreply.github.com>
2016-07-22 23:59:59 +0100

    string operators

---
+++ chapter02/js/string-operator.js
@@ -0,0 +1,8 @@
+//string operators
+var greeting = 'Howdy';
+var name = 'Tom';
+
+var welcomeMessage = greeting + name + '!';
+
+var el = document.getElementById('greeting');
+el.textContent = welcomeMessage;
