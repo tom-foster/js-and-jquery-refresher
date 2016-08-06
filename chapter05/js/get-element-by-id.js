81991618ff940d2b0c0666b5df7a0b28ba39329e
tom-foster <tom-foster@users.noreply.github.com>
2016-08-06 23:51:07 +0100

    chapter05 added cs and getElementById example

---
+++ chapter05/js/get-element-by-id.js
@@ -0,0 +1,9 @@
+//get element by id
+//chapter 05
+//tf 06/08/26
+
+//selecting elements using id attributes
+var el = document.getElementById('one');
+
+//Change  the value of the class atribute.
+el.className = 'cool';
