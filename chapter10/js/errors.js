5818a7dccb63e8fdcc81ca539b164eb984eeb613
tom-foster <tom-foster@users.noreply.github.com>
2016-09-17 19:12:10 +0100

    start chapter 10, files, folders, and a page with an error

---
+++ chapter10/js/errors.js
@@ -0,0 +1,8 @@
+//demonstrating errors in the console.
+// check edge / firefox / chrome differences // safari too if your on a mac
+//tf 17/09/16
+
+console.log("Something's wrong...");
+
+var $form;
+$form = jQuery(#wrong-syntax);
